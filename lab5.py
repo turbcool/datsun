ca67415843124ad2df29433bfe760f3704028d11
Ilya Naydanov <turbcool@gmail.com>
2019-11-08 19:49:13 +0500

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -3,12 +3,13 @@ expertMarks = [
     [2,4,1,5,6,3]
 ]
 
-n=6
-l = []
+n=len(expertMarks[0])
+#marksMod = [n-mark for e in expertMarks for mark in e]
+marksMod = []
 for e in expertMarks:
-    l.append([n-mark for mark in e])
+    marksMod.append([n-mark for mark in e])
     
-mSum = [0] * len(marksMod[0])
+mSum = [0] * n
 for e in marksMod:
     for i, m in enumerate(e):
         mSum[i] = mSum[i] + e[i]
@@ -21,23 +22,3 @@ print('Веса целей:', weights)
 maxW = max(weights)
 mMaxI = [i for i, x in enumerate(weights) if x == maxW]
 print('Цели с наибольшим весом:', mMaxI)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
